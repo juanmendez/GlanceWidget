842419101a7833ac4e69a5343da92de0a1dc6bad
Juan Mendez <juan.mendez@tutanota.com>
2023-12-04 20:27:41 -0600

    02: initial composable content

--- app/src/main/java/com/example/glance/MyAppWidget.kt
+++ app/src/main/java/com/example/glance/MyAppWidget.kt
@@ -1,10 +1,20 @@
 package com.example.glance
 
 import android.content.Context
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
 
 class MyAppWidget : GlanceAppWidget() {
 
@@ -16,7 +26,28 @@ class MyAppWidget : GlanceAppWidget() {
 
         provideContent {
             // create your AppWidget here
-            Text("Hello World")
+            MyContent()
+        }
+    }
+
+    @Composable
+    private fun MyContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize(),
+            verticalAlignment = Alignment.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
+            Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button(
+                    text = "Home",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+                Button(
+                    text = "Work",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+            }
         }
     }
 }
