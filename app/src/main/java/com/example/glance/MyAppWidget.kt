e0f6f1ddd88f419213b3cf5ba92485bea88a16c2
Juan Mendez <juan.mendez@tutanota.com>
2023-12-04 20:17:03 -0600

    00.initial widget setup

---
+++ app/src/main/java/com/example/glance/MyAppWidget.kt
@@ -0,0 +1,22 @@
+package com.example.glance
+
+import android.content.Context
+import androidx.compose.material3.Text
+import androidx.glance.GlanceId
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+
+class MyAppWidget : GlanceAppWidget() {
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+
+        // In this method, load data needed to render the AppWidget.
+        // Use `withContext` to switch to another thread for long running
+        // operations.
+
+        provideContent {
+            // create your AppWidget here
+            Text("Hello World")
+        }
+    }
+}
