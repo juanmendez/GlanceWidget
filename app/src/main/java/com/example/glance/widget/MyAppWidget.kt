cc20279ec510f233075e85236fc51d0a45c27dbe
Juan Mendez <juan.mendez@tutanota.com>
2023-12-05 20:06:32 -0600

    05: responsive size

--- app/src/main/java/com/example/glance/widget/MyAppWidget.kt
+++ app/src/main/java/com/example/glance/widget/MyAppWidget.kt
@@ -7,17 +7,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.Image
 import androidx.glance.ImageProvider
+import androidx.glance.LocalSize
 import androidx.glance.action.ActionParameters
 import androidx.glance.action.actionParametersOf
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.action.actionStartService
 import androidx.glance.appwidget.lazy.LazyColumn
@@ -44,7 +47,19 @@ class MyAppWidget : GlanceAppWidget() {
     companion object {
         const val TAG = "MyAppWidget"
         val DESTINATION_KEY = ActionParameters.Key<String>("Hello")
+
+        private val SMALL_SQUARE = DpSize(150.dp, 100.dp)
+        private val MEDIUM_RECTANGLE = DpSize(200.dp, 100.dp)
+        private val BIG_SQUARE = DpSize(250.dp, 250.dp)
     }
+
+    override val sizeMode = SizeMode.Responsive(
+        setOf(
+            SMALL_SQUARE,
+            MEDIUM_RECTANGLE,
+            BIG_SQUARE
+        )
+    )
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
 
@@ -61,9 +76,10 @@ class MyAppWidget : GlanceAppWidget() {
     @Composable
     private fun MyContent() {
         val repository = remember { Repository }
-        val states = remember { repository.getStates() }
+        val state by repository.getStates().collectAsState(LOADING)
 
-        val state by states.collectAsState(LOADING)
+        // Size will be one of the sizes defined above.
+        val size = LocalSize.current
 
         Box(modifier = GlanceModifier.background(R.color.teal_200)) {
             when (state) {
@@ -78,7 +94,13 @@ class MyAppWidget : GlanceAppWidget() {
                 }
 
                 SUCCESS -> {
-                    SuccessContent(repository)
+                    if (size.width >= BIG_SQUARE.width) {
+                        BigContent(repository)
+                    } else if (size.width >= MEDIUM_RECTANGLE.width) {
+                        MediumContent(repository)
+                    } else {
+                        SmallContent(repository)
+                    }
                 }
 
                 ERROR -> {
@@ -94,8 +116,68 @@ class MyAppWidget : GlanceAppWidget() {
     }
 
     @Composable
-    private fun SuccessContent(repository: Repository) {
-        var count by remember { repository.count }
+    private fun SmallContent(repository: Repository) {
+        val count by remember { repository.count }
+
+        val scope = rememberCoroutineScope()
+
+        // Make column to be scrollable
+        LazyColumn(
+            modifier = GlanceModifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Button(text = "refresh, count ${count}", onClick = {
+                    scope.launch {
+                        count
+                    }
+                })
+            }
+        }
+    }
+
+    @Composable
+    private fun MediumContent(repository: Repository) {
+        val count by remember { repository.count }
+
+        val scope = rememberCoroutineScope()
+
+        // Make column to be scrollable
+        LazyColumn(
+            modifier = GlanceModifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            item {
+                Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Button(
+                        text = "Go Home",
+                        onClick = actionStartActivity<MainActivity>()
+                    )
+                    Button(
+                        text = "Work",
+                        onClick = actionStartActivity<MainActivity>()
+                    )
+                    Button(
+                        text = "Sync",
+                        onClick = actionStartService<SyncService>(isForegroundService = true)
+                    )
+                }
+            }
+
+            item {
+                Button(text = "refresh, count ${count}", onClick = {
+                    scope.launch {
+                        count
+                    }
+                })
+            }
+        }
+    }
+
+    @Composable
+    private fun BigContent(repository: Repository) {
+        val count by remember { repository.count }
 
         val scope = rememberCoroutineScope()
 
@@ -146,11 +228,10 @@ class MyAppWidget : GlanceAppWidget() {
             }
 
             item {
-                Button(text = "Refresh", onClick = {
+                Button(text = "refresh", onClick = {
                     scope.launch {
                         count
                     }
-
                 })
             }
         }
